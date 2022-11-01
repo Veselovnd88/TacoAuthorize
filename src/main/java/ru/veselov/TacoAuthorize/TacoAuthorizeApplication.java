205db3f679895247ddf9093f93f389a1c5bfe759
Veselov Nikolay <veselovnd@gmail.com>
2022-11-01 20:36:36 +0300

    Initial commit

---
+++ src/main/java/ru/veselov/TacoAuthorize/TacoAuthorizeApplication.java
@@ -0,0 +1,13 @@
+package ru.veselov.TacoAuthorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoAuthorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoAuthorizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/veselov/TacoAuthorize/TacoAuthorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.veselov.TacoAuthorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoAuthorizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
